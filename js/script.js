77c32d826f843291c09c0fe2b7b948f35ff70b84
Archilg456 <gabaidze555@gmail.com>
2022-12-28 17:08:26 +0400

    new style

--- js/script.js
+++ js/script.js
@@ -6,4 +6,34 @@ for (i = 0; i < accordion.length; i++ ){
     } ) 
 }
 
-let loggin = document.getElementById('#loggin');
+// log _ reg
+let log_btn = document.querySelector('.enter_btn');
+let log_reg_js = document.querySelector('.log_reg');
+
+log_btn.addEventListener("click", function(){
+    log_btn.classList.toggle("enter_btn__avtive")
+
+    log_reg_js.classList.toggle('log_reg_active')
+})
+
+// function openLoginForm(){
+//     document.body.classList.add("showLoginForm");
+//   }
+//   function closeLoginForm(){
+//     document.body.classList.remove("showLoginForm");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
